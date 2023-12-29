9c5e79a425bfcc27e1dc7d64d0e547a46a5577e8
Akram Khan <akram.bizup@gmail.com>
2023-12-30 02:19:31 +0530

    up

--- main.go
+++ main.go
@@ -11,15 +11,9 @@ func main() {
 	// Enable CORS for all origins
 	router.Use(func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
 		c.Next()
 	})
 
